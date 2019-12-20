46bae08b711bc61c68156f1b5f4d9c99c82d27a2
Kevin Messiaen <kmessiaen@nextoo.fr>
2019-12-20 15:51:10 +0100

    order cities

--- src/main/java/com/messiaen/dropshipping/repository/CityRepository.java
+++ src/main/java/com/messiaen/dropshipping/repository/CityRepository.java
@@ -10,7 +10,7 @@ import java.util.Collection;
 @Repository
 public interface CityRepository extends JpaRepository<City, Integer> {
 
-    @Query("SELECT c FROM City c WHERE c.postCodes LIKE CONCAT(:postCode, '%') OR c.postCodes LIKE CONCAT('%-', :postCode, '%')")
+    @Query("SELECT c FROM City c WHERE c.postCodes LIKE CONCAT(:postCode, '%') OR c.postCodes LIKE CONCAT('%-', :postCode, '%') ORDER BY c.name ASC")
     Collection<City> findAllByPostCodeStartingWith(String postCode);
 
 }
